d2c1e11c6db5e3ea7b487d4e894050690fa02c74
Alex Ahumada <projectsperminute@gmail.com>
2024-09-04 11:06:28 +0200

    Fixes missing MiniBarChart import

--- components/DataTable/GoalSub.vue
+++ components/DataTable/GoalSub.vue
@@ -5,9 +5,12 @@
         {{ spliceGoalName(goal.goal)[0] }}
       </div>
       <div class="w-24">
-        <MiniBarChart :total_duration="goal.total_duration" :tagged_duration="goal.tagged_duration" />
+        <MiniBarChart
+          :total_duration="goal.total_duration"
+          :tagged_duration="goal.tagged_duration"
+        />
       </div>
-      <div>{{ spliceGoalName(goal.goal)[1]}}</div>
+      <div>{{ spliceGoalName(goal.goal)[1] }}</div>
       <div>
         {{ goal.total_duration }}
       </div>
@@ -16,16 +19,15 @@
 </template>
 
 <script setup lang="ts">
-import MiniBarChart from './miniBarChart.vue';
+import MiniBarChart from "./MiniBarChart.vue";
 
 interface Props {
   data: StatsGoal[];
 }
 
 function spliceGoalName(goal: string) {
-
-  let byword= goal.split(' ');
-  return [byword[0], byword.slice(1).join(' ')]
+  let byword = goal.split(" ");
+  return [byword[0], byword.slice(1).join(" ")];
 }
 
 const props = withDefaults(defineProps<Props>(), {});
