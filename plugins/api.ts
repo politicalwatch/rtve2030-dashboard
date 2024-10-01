56238e683d731cdd27a3228257820b8ec72a29b3
Alex Ahumada <projectsperminute@gmail.com>
2024-10-01 13:42:02 +0200

    Remove storeToRefs from api plugin

--- plugins/api.ts
+++ plugins/api.ts
@@ -5,22 +5,16 @@ export default defineNuxtPlugin({
       // interceptors TBD https://github.com/unjs/ofetch
       onRequest({ request, options }) {
         const authStore = useAuthStore();
-        const { session } = storeToRefs(authStore);
+        const session = authStore.session;
         const headers = (options.headers as HeadersInit) || [];
 
-        if (session.value) {
+        if (session) {
           if (Array.isArray(headers)) {
-            headers.push([
-              "Authorization",
-              `Bearer ${session.value.access_token}`,
-            ]);
+            headers.push(["Authorization", `Bearer ${session.access_token}`]);
           } else if (headers instanceof Headers) {
-            headers.set(
-              "Authorization",
-              `Bearer ${session.value.access_token}`
-            );
+            headers.set("Authorization", `Bearer ${session.access_token}`);
           } else {
-            headers.Authorization = `Bearer ${session.value.access_token}`;
+            headers.Authorization = `Bearer ${session.access_token}`;
           }
         }
       },
