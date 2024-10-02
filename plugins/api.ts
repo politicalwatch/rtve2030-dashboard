a368bd221fdbe32eda8b9be9fcbf3abdaad95693
Alex Ahumada <projectsperminute@gmail.com>
2024-10-02 09:35:12 +0200

    Adds redirect on 401 status

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,35 +1,36 @@
-export default defineNuxtPlugin({
-  setup() {
-    const api = $fetch.create({
-      baseURL: useRuntimeConfig().public.apiBase,
-      // interceptors TBD https://github.com/unjs/ofetch
-      onRequest({ request, options }) {
-        const authStore = useAuthStore();
-        const { session } = storeToRefs(authStore);
+// Use recommended recipe: https://nuxt.com/docs/guide/recipes/custom-usefetch#custom-usefetch
+export default defineNuxtPlugin((nuxtApp) => {
+  const authStore = useAuthStore();
+  const { session } = storeToRefs(authStore);
+
+  const api = $fetch.create({
+    baseURL: useRuntimeConfig().public.apiBase,
+    // interceptors TBD https://github.com/unjs/ofetch
+    onRequest({ request, options, error }) {
+      if (session.value) {
         const headers = (options.headers as HeadersInit) || [];
-
-        if (session.value) {
-          if (Array.isArray(headers)) {
-            headers.push([
-              "Authorization",
-              `Bearer ${session.value.access_token}`,
-            ]);
-          } else if (headers instanceof Headers) {
-            headers.set(
-              "Authorization",
-              `Bearer ${session.value.access_token}`
-            );
-          } else {
-            headers.Authorization = `Bearer ${session.value.access_token}`;
-          }
+        if (Array.isArray(headers)) {
+          headers.push([
+            "Authorization",
+            `Bearer ${session.value.access_token}`,
+          ]);
+        } else if (headers instanceof Headers) {
+          headers.set("Authorization", `Bearer ${session.value.access_token}`);
+        } else {
+          headers.Authorization = `Bearer ${session.value.access_token}`;
         }
-      },
-    });
+      }
+    },
+    async onResponseError({ response }) {
+      if (response.status === 401) {
+        await nuxtApp.runWithContext(() => navigateTo("/login"));
+      }
+    },
+  });
 
-    return {
-      provide: {
-        api,
-      },
-    };
-  },
+  return {
+    provide: {
+      api,
+    },
+  };
 });
